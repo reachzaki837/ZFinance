49ca3c311f50cd7920c27b4161c5cdd4bd64dba3
Mohammed Zaki <reachzaki837@gmail.com>
2026-08-27 11:45:56 +0530

    gitignore file updated

--- src/store/AppProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { type ReactNode } from "react";
-import { AppContext, useAppState } from "./useStore";
-
-export function AppProvider({ children }: { children: ReactNode }) {
-  const state = useAppState();
-  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
-}
